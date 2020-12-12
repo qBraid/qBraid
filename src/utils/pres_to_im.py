8cf46784bed6f3be917c3690c38795327db324ff
Kanav Setia <kanav@Kanavs-MacBook-Air.local>
2020-12-12 17:50:29 -0500

    removed few bugs from the script for converting presentation to nb

--- src/utils/pres_to_im.py
+++ src/utils/pres_to_im.py
@@ -4,22 +4,27 @@ import sys
 import os
 
 nb = nbf.v4.new_notebook()
+# print(os.path.splitext(sys.argv[1])[0])
 
-print(os.path.splitext(sys.argv[1])[0])
+pdf_filename = os.path.basename(sys.argv[1]) 
 
-dir_name = os.path.splitext(sys.argv[1])[0]
-os.mkdir(dir_name)
+dir_path = os.path.splitext(sys.argv[1])[0]
+os.mkdir(dir_path)
+dir_name = os.path.splitext(pdf_filename)[0]
+
 slide_list = convert_from_path(str(sys.argv[1]))
 cell_list = []
 i = 1
 for slide in slide_list:
     slide_str = 'slide'+str(i)
-    slide.save(dir_name+'/'+slide_str+'.png', 'PNG')
+    slide.save(dir_path+'/'+slide_str+'.png', 'PNG')
     i=i+1
-    path = '''"'''+dir_name+'/'+slide_str+'.png'+'''"'''
+    path = '''"'''+'./'+dir_name+'/'+slide_str+'.png'+'''"'''
     text = '<img src='+ path +' height="600" width="700" />'
     cell_list.append(nbf.v4.new_markdown_cell(text))
 
 nb['cells'] = cell_list
 
-nbf.write(nb, 'test.ipynb')
+nb_filename = os.path.splitext(pdf_filename)[0]+'.ipynb'
+
+nbf.write(nb, os.path.dirname(dir_path)+'/'+nb_filename)
