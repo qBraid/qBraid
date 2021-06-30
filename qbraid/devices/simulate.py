baf19199709b267bbcd3f75df1004bf4166c96b3
Ryan Hill <ryanjh88@gmail.com>
2021-06-30 15:27:21 -0700

    add imports

--- qbraid/devices/simulate.py
+++ qbraid/devices/simulate.py
@@ -2,3 +2,7 @@ from cirq.circuits import Circuit as CirqCircuit
 from cirq.devices.device import Device as CirqDevice
 from cirq.sim.simulator import TSimulationTrialResult as CirqSimulateResult
 from pandas import DataFrame as CirqSampleResult
+
+
+
+
