b00f23b435e36ed82842d56da35d0b4ad196e630
rryoung98 <ritsukiyoung98@gmail.com>
2021-06-30 10:57:29 -0400

    Tests pre gate implementation

--- tests/circuits/__init__.py
+++ tests/circuits/__init__.py
@@ -1,3 +1,3 @@
-from qbraid.transpiler import qbraid_wrapper
+from qbraid.transpiler.transpiler import qbraid_wrapper
 from qbraid.devices.execute import execute
 from qbraid.devices.getdevice import get_qbraid_device
--- tests/circuits/test_circuit.py
+++ tests/circuits/test_circuit.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 # All rights reserved-2019©.
 import numpy as np
+import pytest
 
+
+import qiskit
+qiskit.qiskit.circuit.measure.CircuitError
 
 ## SET BACKEND
 import matplotlib as mpl
@@ -12,8 +16,19 @@ from qbraid.circuits.circuit import Circuit
 from qbraid.circuits.moment import Moment
 from qbraid.circuits.instruction import Instruction
 
-def test_creating_circuit():
-    circuit = Circuit(num_qubits=3,name="test_circuit")
-    print(circuit)
+
+def circuit():
+    return Circuit(num_qubits=3,name="test_circuit")
     
-test_creating_circuit()
+
+@pytest.mark.parametrize('circuit_param, expected', [
+        (circuit(), {'_qubits':[0,1,2],'_moments':[],'name':'test_circuit'}),])
+def test_creating_circuit(circuit_param,expected):
+    # check class parameters
+    assert circuit_param.__dict__ == expected
+
+@pytest.mark.parametrize('circuit_param, expected', [
+        (circuit(), {'_qubits':[0,1,2],'_moments':[],'name':'test_circuit'}),])
+def test_add_moment(circuit_param,expected):
+    # check class parameters
+    assert circuit_param.__dict__ == expected
