f596ae69f4d4ba08ff5361d699d39c71f2bf7765
rryoung98 <ritsukiyoung98@gmail.com>
2021-06-30 17:30:26 -0400

    fixed some import issues, etc

--- tests/circuits/test_circuit.py
+++ tests/circuits/test_circuit.py
@@ -9,6 +9,7 @@ import matplotlib as mpl
 mpl.use("TkAgg")
 
 
+import qbraid.circuits.update_rule
 from qbraid.circuits.circuit import Circuit
 from qbraid.circuits.moment import Moment
 from qbraid.circuits.instruction import Instruction
@@ -35,13 +36,18 @@ MOMENTS
 CIRCUITS
 """
 @pytest.mark.parametrize('circuit_param, expected', [
-        (circuit(), {'_qubits':[0,1,2],'_moments':[],'name':'test_circuit'}),])
+        (circuit(), {'_qubits':[0,1,2],'_moments':[],'name':'test_circuit','update_rule': qbraid.circuits.update_rule.UpdateRule.NEW_THEN_INLINE}),])
 def test_creating_circuit(circuit_param,expected):
     # check class parameters
-    assert circuit_param.__dict__ == expected
+    dict = circuit_param.__dict__
+    check_qubit = []
+    for qubit_obj in dict['_qubits']:
+        check_qubit.append(qubit_obj._index)
+    dict['_qubits'] = check_qubit
+    assert dict == expected
 
 @pytest.mark.parametrize('circuit_param, expected', [
-        ('_moments':[], '_moments':[]),])
+        ('_moments'==[]),])
 def test_add_moment(moment,circuit_param, expected):
     # check class parameters
     circuit_param.append(moment)
