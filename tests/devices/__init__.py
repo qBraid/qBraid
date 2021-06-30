9f0e73d28b58c3a1cd6936a90a8e61e47feb306f
rryoung98 <ritsukiyoung98@gmail.com>
2021-06-29 23:22:01 -0400

    fixed imports of qbraid_wrapper

--- tests/devices/__init__.py
+++ tests/devices/__init__.py
@@ -1,3 +1,3 @@
-from qbraid.circuits.transpiler import qbraid_wrapper
+from qbraid.transpiler import qbraid_wrapper
 from qbraid.devices.execute import execute
 from qbraid.devices.getdevice import get_qbraid_device
