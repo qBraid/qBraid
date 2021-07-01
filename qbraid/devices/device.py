1f185e264d19205833d13666e5aed937b807250f
Ryan Hill <ryanjh88@gmail.com>
2021-06-30 19:31:57 -0700

    first draft of abstract device class

--- qbraid/devices/device.py
+++ qbraid/devices/device.py
@@ -1,47 +1,91 @@
-from abc import ABC, abstractmethod
-from qbraid.transpiler.circuit import AbstractCircuitWrapper
-from typing import Optional
+"""Device abstract interface."""
+
+from abc import ABC
+from abc import abstractmethod
 
 
 class AbstractDevice(ABC):
 
-    def __init__(self):
+    def __init__(self, name, provider, **fields):
+        """Initialize a device class
+        Args:
+            name (str): a qBraid supported device
+            provider (str): the provider that this device comes from
+            fields: kwargs for the values to use to override the default options.
+        Raises:
+            AttributeError: if input field not a valid options
+        """
+        self._name = name
+        self._provider = provider
+        self._options = self._default_options()
+        self._configuration = None
+        if fields:
+            for field in fields:
+                if field not in self._options.data:
+                    raise AttributeError("Options field %s is not valid for this device" % field)
+            self._options.update_config(**fields)
 
-        self._name = None
-        self._vendor = None
+    @classmethod
+    @abstractmethod
+    def _default_options(cls):
+        """Return the default options
+        This method will return a :class:`qiskit.providers.Options`
+        subclass object that will be used for the default options. These
+        should be the default parameters to use for the options of the
+        backend.
+        Returns:
+            qiskit.providers.Options: A options object with default values set
+        """
+        pass
+
+    def set_options(self, **fields):
+        """Set the options fields for the device.
+        This method is used to update the options of a device. If
+        you need to change any of the options prior to running just
+        pass in the kwarg with the new value for the options.
+        Args:
+            fields: The fields to update the options
+        Raises:
+            AttributeError: If the field passed in is not part of the options
+        """
+        for field in fields:
+            if not hasattr(self._options, field):
+                raise AttributeError("Options field %s is not valid for this " "device" % field)
+        self._options.update_options(**fields)
+
+    def configuration(self):
+        """Return the device configuration.
+        Returns:
+            dict: the configuration for the device. If the device does not support properties,
+            it returns ``None``.
+        """
+        return self._configuration
 
     @property
     def name(self):
+        """Return the device name.
+        Returns:
+            str: the name of the device.
+        """
         return self._name
 
     @property
-    def vendor(self):
-        return self._vendor
+    def provider(self):
+        """Return the device provider.
+        Returns:
+            str: the provider responsible for the device.
+        """
+        return self._provider
 
     @property
-    def device_type(self):
-        # either simultator, noise simulator, ion-trap, superconducting
-        raise NotImplementedError
-
-    @property
-    def calibration_data(self):
-        raise NotImplementedError
-
-    @property
-    def num_qubits(self):
-        raise NotImplementedError
-
-    @property
-    def decay_times(self):
-        """Returns a dictionatary of qubits with each data"""
-        raise NotImplementedError
+    def options(self):
+        """Return the options for the device
+        The options of a device are the dynamic parameters defining
+        how the device is used. These are used to control the :meth:`run`
+        method.
+        """
+        return self._options
 
     @abstractmethod
-    def validate_circuit(self):
-        """Checks if a qbraid circuit can be run on this device"""
-        raise NotImplementedError
-
-    @abstractmethod
-    def run(self, circuit: AbstractCircuitWrapper, shots: Optional[int]):
-        raise NotImplementedError
-
+    def run(self, run_input, **options):
+        pass
