1968adddf25625097dfbdf129119e40df536fcc7
gwjacobson <graemejacobson@gmail.com>
2021-07-06 14:23:36 -0500

    added measurement class

---
+++ qbraid/circuits/library/standard_gates/measure.py
@@ -0,0 +1,7 @@
+from ...gate import Gate
+from typing import Optional
+
+class Measure(Gate):
+    
+    def __init__(self, global_phase: Optional[float]):
+        super().__init__("measure", num_qubits=1, params=[], global_phase=global_phase)
