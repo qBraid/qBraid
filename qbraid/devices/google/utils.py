da8b19f5dc7a96110ec663ab549d5b17ead97d5a
Ryan Hill <ryanjh88@gmail.com>
2021-07-23 12:41:43 -0700

    unversioned files

---
+++ qbraid/devices/google/utils.py
@@ -0,0 +1,27 @@
+from cirq_google import Sycamore, Sycamore23, Bristlecone, Foxtail
+
+AQT_DEVICES = {
+    'AQT': None,  # AQTSampler(url, access_token=access_token),
+}
+
+GOOGLE_DEVICES = {
+    'Sycamore': Sycamore,
+    'Sycamore23': Sycamore23,
+    'Bristlecone': Bristlecone,
+    'Foxtail': Foxtail
+}
+
+IONQ_DEVICES = {
+    'ionQdevice': None,  # ionq.Service(),
+}
+
+PASQAL_DEVICES = {
+    'PasqalVirtualDevice': None,  # PasqalVirtualDevice,
+}
+
+CIRQ_PROVIDERS = {
+    'AQT': AQT_DEVICES,
+    'Google': GOOGLE_DEVICES,
+    'IonQ': IONQ_DEVICES,
+    'Pasqal': PASQAL_DEVICES,
+}
---
+++ qbraid/devices/utils.py
@@ -0,0 +1,57 @@
+from tabulate import tabulate
+from typing import Optional
+
+from .aws.device import BraketDeviceWrapper
+from .google.device import CirqSamplerWrapper
+from .ibm.device import QiskitBackendWrapper
+from .device import DeviceLikeWrapper
+from .exceptions import DeviceError
+from .aws.utils import BRAKET_PROVIDERS
+from .google.utils import CIRQ_PROVIDERS
+from .ibm.utils import QISKIT_PROVIDERS
+
+SUPPORTED_VENDORS = {
+    "AWS": BRAKET_PROVIDERS,
+    "Google": CIRQ_PROVIDERS,
+    "IBM": QISKIT_PROVIDERS,
+}
+
+
+def device_wrapper(
+    name: str, provider: str, vendor: Optional[str] = None, **fields
+) -> DeviceLikeWrapper:
+    """Apply qbraid device wrapper to device from a supported device provider. If vendor is None,
+    it is assumed that the vendor is the same as the provider. If the vendor is not the same as the
+    provider, the vendor must be specified.
+
+    Args:
+        name (str): a quantum hardware device/simulator available through given ``provider``
+        provider (str): a quantum hardware device/simulator provider available through ``vendor``
+        vendor (Optional[str]): a quantum software vendor
+
+    Returns:
+        :class:`~qbraid.devices.device.DeviceWrapper`: a qbraid device wrapper object
+    Raises:
+        :class:`~qbraid.DeviceError`: If `vendor` is not a supported vendor.
+    """
+    if vendor is None:
+        vendor = provider
+
+    if vendor == "AWS":
+        return BraketDeviceWrapper(name, provider, **fields)
+    elif vendor == "Google":
+        return CirqSamplerWrapper(name, provider, **fields)
+    elif vendor == "IBM":
+        return QiskitBackendWrapper(name, provider, **fields)
+    else:
+        raise DeviceError('"{}" is not a supported vendor.'.format(vendor))
+
+
+def get_devices():
+    """Prints all available devices, tabulated by provider and vendor."""
+    device_list = []
+    for vendor_key in SUPPORTED_VENDORS:
+        for provider_key in SUPPORTED_VENDORS[vendor_key]:
+            for device_key in SUPPORTED_VENDORS[vendor_key][provider_key]:
+                device_list.append([vendor_key, provider_key, device_key])
+    print(tabulate(device_list, headers=["Software Vendor", "Device Provider", "Device Name"]))
