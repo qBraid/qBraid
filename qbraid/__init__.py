faf9fc0962a0bd010bb0bfce7cc938e189855e3f
Ryan Hill <ryanjh88@gmail.com>
2021-08-20 20:04:52 -0700

    tox InvocationErrors

--- qbraid/__init__.py
+++ qbraid/__init__.py
@@ -1,6 +1,5 @@
 """This top level module contains the main qBraid public functionality."""
 
-
 from importlib.metadata import entry_points, version
 from qbraid.circuits import Circuit, UpdateRule
 from qbraid.devices import get_devices
