2c8cebca42d54b84d846ff1befb23f4654e2d20f
Ryan <ryanjh88@gmail.com>
2021-10-19 18:17:39 -0500

    zip safe / setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-import setuptools
+from setuptools import setup
 
 with open("qbraid/_version.py") as f:
     version = f.readlines()[-1].split()[-1].strip("\"'")
 
-setuptools.setup(version=version)
+setup(version=version)
