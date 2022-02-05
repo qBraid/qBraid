4fc2e7ac28a10df50e51f23c3832ac165a7534cd
rryoung98 <ritsukiyoung98@gmail.com>
2022-02-05 12:17:12 -0600

    adding pylint on PR

--- qbraid/devices/tests/test_devices.py
+++ qbraid/devices/tests/test_devices.py
@@ -40,7 +40,7 @@ from qbraid.devices.ibm import (
 def device_wrapper_inputs(vendor: str):
     """Returns list of tuples containing all device_wrapper inputs for given vendor."""
     devices = requests.post(
-        "https://api.qbraid.com/api" + "/get-devices", json={}, verify=False
+        os.getenv("API_URL") + "/get-devices", json={}, verify=False
     ).json()
     input_list = []
     for document in devices:
