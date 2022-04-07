f35656371dc2289e2dc5eb3597c64aad53b1b98b
Ryan <ryanjh88@gmail.com>
2022-04-07 12:23:32 -0500

    add draw

--- qbraid/interface/__init__.py
+++ qbraid/interface/__init__.py
@@ -12,8 +12,10 @@ Interface (:mod:`qbraid.interface`)
    convert_to_contiguous
    equal_unitaries
    random_circuit
+   draw
 
 """
 from ._programs import random_circuit
 from .calculate_unitary import equal_unitaries, to_unitary
 from .convert_to_contiguous import convert_to_contiguous
+from .draw_circuit import draw
---
+++ qbraid/interface/draw_circuit.py
@@ -0,0 +1,27 @@
+"""Module for drawing quantum circuit diagrams"""
+
+from qbraid._typing import QPROGRAM
+from qbraid.exceptions import ProgramTypeError
+
+
+def draw(program: QPROGRAM) -> None:
+    """Draws circuit diagram."""
+
+    try:
+        package = program.__module__
+    except AttributeError as err:
+        raise ProgramTypeError(program) from err
+
+    if "qiskit" in package:
+        program.draw()   
+
+    elif "pennylane" in package:
+        print(program.draw())
+
+    elif "braket" in package or "cirq" in package or "pyquil" in package:
+        print(program)
+
+    else:
+        raise ProgramTypeError(program)
+
+    return
