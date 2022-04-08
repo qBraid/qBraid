707b661b1148028eca313a867444522e41222489
Ryan Hill <ryanjh88@gmail.com>
2022-04-08 14:02:12 -0500

    Update _warnings.py

--- qbraid/_warnings.py
+++ qbraid/_warnings.py
@@ -2,7 +2,7 @@
 import warnings
 import urllib3
 
-warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=UserWarning, message="Setuptools is replacing distutils")
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
