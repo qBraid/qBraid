c384c97a56154cbf4559b233fc2a3445c41aac23
poig <43441314+poig@users.noreply.github.com>
2023-02-24 02:59:43 +0800

    fix black

--- qbraid/transpiler/cirq_braket/convert_from_braket.py
+++ qbraid/transpiler/cirq_braket/convert_from_braket.py
@@ -21,7 +21,8 @@ from typing import Dict, List
 import numpy as np
 from braket.circuits import Circuit as BKCircuit
 from braket.circuits import Instruction as BKInstruction
-from braket.circuits import gates as braket_gates, noises as braket_noise_gate
+from braket.circuits import gates as braket_gates
+from braket.circuits import noises as braket_noise_gate
 from cirq import Circuit, LineQubit
 from cirq import ops as cirq_ops
 from cirq import protocols
@@ -94,7 +95,10 @@ def _from_braket_instruction(
     BK_qubits = [int(q) for q in instr.target]
     qubits = [qubit_mapping[x] for x in BK_qubits]
 
-    if not instr.operator.__module__ in (braket_gates.__name__, braket_noise_gate.__name__) or isinstance(instr.operator, braket_gates.PulseGate):
+    if not instr.operator.__module__ in (
+        braket_gates.__name__,
+        braket_noise_gate.__name__,
+    ) or isinstance(instr.operator, braket_gates.PulseGate):
         raise CircuitConversionError(f"Qbraid doesn't support {instr.operator}")
 
     if nqubits == 1:
@@ -179,7 +183,9 @@ def _from_one_qubit_braket_instruction(
     if isinstance(gate, braket_noise_gate.AmplitudeDamping):
         return [cirq_ops.AmplitudeDampingChannel(gate.gamma).on(*qubits)]
     if isinstance(gate, braket_noise_gate.GeneralizedAmplitudeDamping):
-        return [cirq_ops.GeneralizedAmplitudeDampingChannel(gate.probability, gate.gamma).on(*qubits)]
+        return [
+            cirq_ops.GeneralizedAmplitudeDampingChannel(gate.probability, gate.gamma).on(*qubits)
+        ]
     if isinstance(gate, braket_noise_gate.PhaseDamping):
         return [cirq_ops.PhaseDampingChannel(gate.gamma).on(*qubits)]
 
@@ -257,7 +263,7 @@ def _from_two_qubit_braket_instruction(
         return [cirq_ops.ZZPowGate(exponent=gate.angle / np.pi, global_shift=-0.5).on(*qubits)]
     if isinstance(gate, braket_gates.XY):
         return [cirq_ops.ISwapPowGate(exponent=gate.angle / np.pi).on(*qubits)]
-    
+
     # Two-qubit noise gates.
     if isinstance(gate, braket_noise_gate.Kraus):
         return [cirq_ops.KrausChannel(gate._matrices).on(*qubits)]
