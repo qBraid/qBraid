237e1b60f9d70f18d83ebec3607ab269176168d5
Ryan Hill <ryanjh88@gmail.com>
2023-05-05 14:42:36 -0500

    formatting fix

--- qbraid/api/retry.py
+++ qbraid/api/retry.py
@@ -2,12 +2,12 @@
 # Copyright (C) IBM
 #
 # This file is part of the qBraid-SDK.
-# 
+#
 # The qBraid-SDK is free software released under the GNU General Public License v3
 # or later. This specific file, adapted from Qiskit, is dual-licensed under both the
 # Apache License, Version 2.0, and the GPL v3. You may not use this file except in
 # compliance with the applicable license. You may obtain a copy of the Apache License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
 #
 # This file includes code adapted from Qiskit (https://github.com/Qiskit/qiskit-ibm-provider)
--- qbraid/api/session.py
+++ qbraid/api/session.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2023 qBraid
 #
 # This file is part of the qBraid-SDK
-# 
+#
 # The qBraid-SDK is free software released under the GNU General Public License v3
 # or later. You can redistribute and/or modify it under the terms of the GPL v3.
 # See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
