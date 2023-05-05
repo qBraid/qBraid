b70489ca29196b03484eb028148e51675a1f388b
Ryan Hill <ryanjh88@gmail.com>
2023-05-05 14:35:09 -0500

    fix / check options

--- tools/verify_headers.py
+++ tools/verify_headers.py
@@ -8,11 +8,19 @@
 #
 # THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
 
+# pylint: skip-file
+
 """
 Script to verify qBraid copyright file headers
 
 """
 import os
+import sys
+
+# ANSI escape codes
+BLUE = "\033[94m"
+BOLD = "\033[1m"
+RESET = "\033[0m"
 
 header = """# Copyright (C) 2023 qBraid
 #
@@ -26,44 +34,38 @@ header = """# Copyright (C) 2023 qBraid
 """
 
 skip_files = [
-    "api/retry.py",
-    "transpiler/cirq_braket/convert_from_braket.py",
-    "transpiler/cirq_braket/convert_to_braket.py",
-    "transpiler/cirq_braket/tests/test_from_braket.py",
-    "transpiler/cirq_braket/tests/test_to_braket.py",
-    "transpiler/cirq_qasm/qasm_parser.py",
-    "transpiler/cirq_qasm/tests/test_qasm_parser.py",
+    "qbraid/api/retry.py",
+    "qbraid/transpiler/cirq_braket/convert_from_braket.py",
+    "qbraid/transpiler/cirq_braket/convert_to_braket.py",
+    "qbraid/transpiler/cirq_braket/tests/test_from_braket.py",
+    "qbraid/transpiler/cirq_braket/tests/test_to_braket.py",
+    "qbraid/transpiler/cirq_qasm/qasm_parser.py",
+    "qbraid/transpiler/cirq_qasm/tests/test_qasm_parser.py",
 ]
 
-
-def header_exists(file_path):
-    with open(file_path, "r", encoding="ISO-8859-1") as f:
-        content = f.read()
-    return content.startswith(header)
+failed_headers = []
+fixed_headers = []
 
 
-def should_skip(file_path):
-    rel_path = os.path.relpath(file_path, project_directory)
-
-    if rel_path in skip_files:
+def should_skip(file_path, content):
+    if file_path in skip_files:
         return True
 
     if os.path.basename(file_path) == "__init__.py":
-        with open(file_path, "r", encoding="ISO-8859-1") as f:
-            content = f.read()
         return not content.strip()
 
     return False
 
 
-def replace_or_add_header(file_path):
-    if should_skip(file_path):
-        return
-
+def replace_or_add_header(file_path, fix=False):
     with open(file_path, "r", encoding="ISO-8859-1") as f:
         content = f.read()
 
-    if header_exists(file_path):
+    if content.startswith(header) or should_skip(file_path, content):
+        return
+
+    if not fix:
+        failed_headers.append(file_path)
         return
 
     lines = content.splitlines()
@@ -83,19 +85,57 @@ def replace_or_add_header(file_path):
     with open(file_path, "w", encoding="ISO-8859-1") as f:
         f.write(new_content)
 
+    fixed_headers.append(file_path)
 
-def process_files_in_directory(directory):
+
+def process_files_in_directory(directory, fix=False):
+    count = 0
     for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith(".py"):
                 file_path = os.path.join(root, file)
-                replace_or_add_header(file_path)
+                replace_or_add_header(file_path, fix)
+                count += 1
+    return count
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please provide at least one directory as a command-line argument.")
+        sys.exit(1)
+
     script_directory = os.path.dirname(os.path.abspath(__file__))
-    project_directory = os.path.abspath(os.path.join(script_directory, "..", "qbraid"))
-    process_files_in_directory(project_directory)
-    print(
-        "Header added or replaced in all .py files in the specified directory and sub-directories, if applicable."
-    )
+    project_directory = os.path.abspath(os.path.join(script_directory, ".."))
+    skip_files = [os.path.join(project_directory, file_path) for file_path in skip_files]
+
+    fix = "--fix" in sys.argv
+    directories = [arg for arg in sys.argv[1:] if arg != "--fix"]
+
+    checked = 0
+    for directory in directories:
+        full_directory_path = os.path.join(project_directory, directory)
+        if os.path.isdir(full_directory_path):
+            checked += process_files_in_directory(full_directory_path, fix)
+        else:
+            failed_headers.append(full_directory_path)
+            print(f"Directory not found: {full_directory_path}")
+
+    if not fix:
+        if failed_headers:
+            for file in failed_headers:
+                print(f"failed {os.path.relpath(file)}")
+            num_failed = len(failed_headers)
+            s1, s2 = ("", "s") if num_failed == 1 else ("s", "")
+            sys.stderr.write(f"\n{num_failed} file header{s1} need{s2} updating.\n")
+        else:
+            print("All file header checks passed!")
+
+    else:
+        for file in fixed_headers:
+            print(f"fixed {os.path.relpath(file)}")
+        num_fixed = len(fixed_headers)
+        num_ok = checked - num_fixed
+        s_fixed = "" if num_fixed == 1 else "s"
+        s_ok = "" if num_ok == 1 else "s"
+        print("\nAll done! ✨ 🚀 ✨")
+        print(f"{num_fixed} header{s_fixed} fixed, {num_ok} header{s_ok} left unchanged.")
