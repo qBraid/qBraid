08bef5603b378c4819c576ce23c014407399d643
poig <laolianglaoliang@gmail.com>
2023-05-16 09:02:41 +0800

    fix header

--- qbraid/interface/qbraid_qasm/test/test_qasm_utils.py
+++ qbraid/interface/qbraid_qasm/test/test_qasm_utils.py
@@ -1,3 +1,13 @@
+# Copyright (C) 2023 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
 from qbraid.interface.qbraid_qasm.tools import qasm_qubits, qasm_num_qubits, qasm_depth
 from qbraid.interface.qbraid_qasm.circuits import qasm_bell, qasm_shared15
 
