75ef8eabc7f0e9820408d6aba58c544a1f31135f
Harshit Gupta <harshit.11235@gmail.com>
2023-05-27 13:44:48 +0530

    add v1 for the qasm2->3 converter

--- qbraid/interface/qbraid_qasm/test/test_qasm_utils.py
+++ qbraid/interface/qbraid_qasm/test/test_qasm_utils.py
@@ -9,7 +9,12 @@
 # THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
 
 from qbraid.interface.qbraid_qasm.circuits import qasm_bell, qasm_shared15
-from qbraid.interface.qbraid_qasm.tools import qasm_depth, qasm_num_qubits, qasm_qubits
+from qbraid.interface.qbraid_qasm.tools import (
+    convert_to_qasm_3,
+    qasm_depth,
+    qasm_num_qubits,
+    qasm_qubits,
+)
 
 
 def test_qasm_qubits():
@@ -28,3 +33,125 @@ def test_qasm_depth():
     """test calcualte qasm depth"""
     assert qasm_depth(qasm_bell()) == 2
     assert qasm_depth(qasm_shared15()) == 22
+
+
+def _check_output(output, expected):
+    for line1, line2 in zip(output.splitlines(), expected.splitlines()):
+        assert line1.strip() == line2.strip()
+
+
+def test_convert_to_qasm_3():
+    """test the conversion of qasm 2 to 3"""
+
+    # 1. qubit statement conversion
+    test_qregs = """OPENQASM 2.0;
+    qreg q[1] ;
+    qreg qubits  [10]   ;
+    creg c[1];
+    creg bits   [12]   ;
+    """
+    test_qregs_expected = """OPENQASM 3.0;
+    qubit[1] q;
+    qubit[10] qubits;
+    bit[1] c;
+    bit[12] bits;"""
+
+    _check_output(convert_to_qasm_3(test_qregs), test_qregs_expected)
+
+    # 2. Measure statement conversion
+    test_measure = """
+    OPENQASM 2.0;
+    qreg q[2];
+    creg c[2];
+    measure q->c;
+    measure q[0] -> c[1];
+    """
+
+    test_measure_expected = """
+    OPENQASM 3.0;
+    qubit[2] q;
+    bit[2] c;
+    c = measure q;
+    c[1] = measure q[0];
+    """
+    _check_output(convert_to_qasm_3(test_measure), test_measure_expected)
+
+    # 3. Opaque comment conversion
+    test_opaque = """
+    OPENQASM 2.0;
+    qreg q[2];
+    creg c[2];
+    opaque custom_gate (a,b,c) p,q,r;
+    """
+
+    test_opaque_expected = """
+    OPENQASM 3.0;
+    qubit[2] q;
+    bit[2] c;
+    // opaque custom_gate (a,b,c) p,q,r;
+    """
+
+    _check_output(convert_to_qasm_3(test_opaque), test_opaque_expected)
+
+    # 4. std header change
+    test_header = """
+    OPENQASM 2.0;
+    include "qelib1.inc";
+    qreg q[1];
+    """
+
+    test_header_expected = """
+    OPENQASM 3.0;
+    include "stdgates.inc";
+    qubit[1] q;
+    """
+    _check_output(convert_to_qasm_3(test_header), test_header_expected)
+
+    # 5. Fully random test
+    test_random = """
+    OPENQASM 2.0;
+    include "qelib1.inc";
+    gate r(param0,param1) q0 { u3(3.1008262509080406,4.42985566593216,-4.42985566593216) q0; }
+    qreg q[3];
+    creg c[3];
+    cy q[2],q[1];
+    s q[0];
+    cp(3.0004583704185523) q[2],q[0];
+    t q[1];
+    crz(4.1306355067715455) q[2],q[0];
+    rz(0.8430458524794254) q[1];
+    cy q[1],q[2];
+    u(4.406951880713489,5.860482620731477,1.4150079480777737) q[0];
+    cu(1.101924061832913,5.126072002075184,5.423867802047752,0.9224141552349424) q[2],q[0];
+    r(3.1008262509080406,6.0006519927270565) q[1];
+    swap q[2],q[1];
+    u1(4.917501631124593) q[0];
+    measure q[0] -> c[0];
+    measure q[1] -> c[1];
+    measure q[2] -> c[2];
+    """
+
+    test_random_expected = """ 
+    OPENQASM 3.0;
+    include "stdgates.inc";
+    gate r(param0,param1) q0 { u3(3.1008262509080406,4.42985566593216,-4.42985566593216) q0; }
+    qubit[3] q;
+    bit[3] c;
+    cy q[2],q[1];
+    s q[0];
+    cp(3.0004583704185523) q[2],q[0];
+    t q[1];
+    crz(4.1306355067715455) q[2],q[0];
+    rz(0.8430458524794254) q[1];
+    cy q[1],q[2];
+    u(4.406951880713489,5.860482620731477,1.4150079480777737) q[0];
+    cu(1.101924061832913,5.126072002075184,5.423867802047752,0.9224141552349424) q[2],q[0];
+    r(3.1008262509080406,6.0006519927270565) q[1];
+    swap q[2],q[1];
+    u1(4.917501631124593) q[0];
+    c[0] = measure q[0];
+    c[1] = measure q[1];
+    c[2] = measure q[2];
+    """
+
+    _check_output(convert_to_qasm_3(test_random), test_random_expected)
--- qbraid/interface/qbraid_qasm/tools.py
+++ qbraid/interface/qbraid_qasm/tools.py
@@ -16,6 +16,7 @@ import re
 
 import numpy as np
 from cirq.circuits import Circuit
+from qiskit.circuit import QuantumCircuit
 
 from qbraid.transpiler.cirq_qasm.qasm_conversions import from_qasm, to_qasm
 
@@ -54,3 +55,92 @@ def _convert_to_contiguous_qasm(qasmstr: str, rev_qubits=False) -> QASMType:
 def _unitary_from_qasm(qasmstr: QASMType) -> np.ndarray:
     """Return the unitary of the QASM"""
     return from_qasm(qasmstr).unitary()
+
+
+def _build_qasm_3_reg(line: str, qreg_type: bool) -> QASMType:
+    """Helper function to build openqasm 3 register statements
+
+    Args:
+        line (str): openqasm 2  regdecl statement
+        qreg_type (bool): whether a qreg or creg type statement
+
+    Returns:
+        str : openqasm 3 qubits / bits declaration
+    """
+    reg_keyword_len = 4
+    line = line[reg_keyword_len:]
+    elements = line.split("[")
+    reg_name = elements[0].strip()
+    reg_size = elements[1].split("]")[0].strip()
+    result = "qubit" if qreg_type else "bit"
+    result += f"[{reg_size}] {reg_name};\n"
+    return result
+
+
+def _build_qasm_3_measure(line: str) -> QASMType:
+    """Helper function to build openqasm 3 measure string
+
+    Args:
+        line (str): openqasm 2 measure statement
+
+    Returns:
+        str:  openqasm 3 measure statement
+    """
+    measure_keyword_len = 7
+    line = line[measure_keyword_len:]
+    elements = line.split("->")
+    qubits_name = elements[0].replace(" ", "")
+    bits_name = elements[1].split(";")[0].replace(" ", "")
+
+    return f"{bits_name} = measure {qubits_name};\n"
+
+
+def _change_to_qasm_3(line: str) -> QASMType:
+    """Function to change an openqasm 2 line to openqasm 3
+
+    Args:
+        line (str): an openqasm 2 line
+
+    Returns:
+        str: corresponding openqasm 3 line
+    """
+    # if openqasm header
+    line = line.lstrip()
+    if line.startswith("OPENQASM"):
+        return "OPENQASM 3.0;\n"
+    # standard gate header
+    if "qelib1.inc" in line:
+        return 'include "stdgates.inc";\n'
+    if line.startswith("qreg"):
+        return _build_qasm_3_reg(line, qreg_type=True)
+    if line.startswith("creg"):
+        return _build_qasm_3_reg(line, qreg_type=False)
+    if line.startswith("measure"):
+        return _build_qasm_3_measure(line)
+    if line.startswith("opaque"):
+        # as opaque is ignored by openqasm 3 add it as a comment
+        return "// " + line + "\n"
+
+    return line + "\n"
+
+
+def convert_to_qasm_3(qasm_2_str: str):
+    """Convert a QASM 2.0 string to QASM 3.0 string
+
+    Args:
+        qasm_2_str (str): QASM 2.0 string
+    """
+
+    try:
+        # use inbuilt method to check validity
+        _ = QuantumCircuit.from_qasm_str(qasm_2_str)
+    except Exception as e:
+        raise ValueError("Invalid QASM 2.0 string") from e
+
+    qasm_3_str = ""
+
+    # safe to assume that we have a newline separated string
+    for line in qasm_2_str.splitlines():
+        line = _change_to_qasm_3(line)
+        qasm_3_str += line
+    return qasm_3_str
