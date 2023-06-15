a2255ed228ac8de0068148f12147131276b10c1c
Ryan Hill <ryanjh88@gmail.com>
2023-06-15 15:24:28 -0500

    import order

--- qbraid/_warnings.py
+++ qbraid/_warnings.py
@@ -35,8 +35,8 @@ def _check_version():
 
     # pylint: disable=import-outside-toplevel
     from ._version import __version__ as version_local
-    from .api.session import QbraidSession
     from .api.job_api import _running_in_lab
+    from .api.session import QbraidSession
 
     if not _running_in_lab():
         return
