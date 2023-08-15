77a88111a08ad6239ac15564af6c13bf147149ec
Pranav Kakhandiki <pranavkakhandiki@Pranavs-MacBook-Pro-2.local>
2023-08-15 13:32:39 -0400

    Fixed formatting, added test cases, variable name

--- qbraid/devices/result.py
+++ qbraid/devices/result.py
@@ -17,7 +17,7 @@ from abc import ABC, abstractmethod
 from qiskit.visualization import plot_histogram
 
 
-def _format_counts(raw_counts: dict, removeZeros = True) -> dict:
+def _format_counts(raw_counts: dict, remove_zeros=True) -> dict:
     """Formats, sorts, and adds missing bit indicies to counts dictionary
     Can pass in a 'removeZeros' parameter to decide whether to plot the non-zero counts
     For example:
@@ -30,15 +30,15 @@ def _format_counts(raw_counts: dict, removeZeros = True) -> dict:
         {'00': 46, '01': 0, '10': 79, '11': 13}
 
     """
-    #method to remove all zero count results
+
+    # method to remove all zero count results
     def remove_zero_values(dictionary):
         keys_to_remove = [key for key, value in dictionary.items() if value == 0]
-        
+
         for key in keys_to_remove:
             del dictionary[key]
-        
-        return dictionary
 
+        return dictionary
 
     # Remove spaces from keys
     counts = {key.replace(" ", ""): value for key, value in raw_counts.items()}
@@ -47,11 +47,10 @@ def _format_counts(raw_counts: dict, removeZeros = True) -> dict:
     num_bits = max(len(key) for key in counts)
     all_keys = [format(i, "0" + str(num_bits) + "b") for i in range(2**num_bits)]
 
-    #removing zero value counts
+    # removing zero value counts
     final_counts = {key: counts.get(key, 0) for key in sorted(all_keys)}
-    if removeZeros:
+    if remove_zeros:
         final_counts = remove_zero_values(final_counts)
-    
     return final_counts
 
 
@@ -74,16 +73,16 @@ class ResultWrapper(ABC):
     def raw_counts(self):
         """Returns raw histogram data of the run"""
 
-    def measurement_counts(self):
+    def measurement_counts(self, remove_zeros=True):
         """Returns the sorted histogram data of the run"""
         raw_counts = self.raw_counts()
         if isinstance(raw_counts, dict):
-            return _format_counts(raw_counts)
+            return _format_counts(raw_counts, remove_zeros)
         return [_format_counts(counts) for counts in raw_counts]
 
-    def plot_counts(self):
+    def plot_counts(self, remove_zeros=True):
         """Plot histogram of measurement counts"""
-        counts = self.measurement_counts()
+        counts = self.measurement_counts(remove_zeros)
         if isinstance(counts, dict):
             return plot_histogram(counts)
         return [plot_histogram(count) for count in counts]
