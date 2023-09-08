bdb411107907fc834285ca9689da64bc3062447a
Ryan Hill <ryanjh88@gmail.com>
2023-09-08 17:00:34 -0500

    add unit tests (#384)

--- tests/providers/test_ibm_provider.py
+++ tests/providers/test_ibm_provider.py
@@ -13,10 +13,15 @@ Unit tests for working with IBM provider
 
 """
 import os
+from unittest.mock import Mock
 
 import pytest
+from qiskit import QuantumCircuit
 from qiskit_ibm_provider import IBMProvider
+from qiskit_ibm_provider.job.ibm_circuit_job import IBMCircuitJob
 
+from qbraid import device_wrapper
+from qbraid.providers.exceptions import JobError
 from qbraid.providers.ibm.provider import ibm_least_busy_qpu, ibm_provider, ibm_to_qbraid_id
 
 # Skip tests if IBM/AWS account auth/creds not configured
@@ -51,3 +56,28 @@ def test_ibm_least_busy():
     """Test returning qbraid ID of least busy IBMQ QPU."""
     qbraid_id = ibm_least_busy_qpu()
     assert qbraid_id[:6] == "ibm_q_"
+
+
+@pytest.mark.skipif(skip_remote_tests, reason=REASON)
+def test_retrieving_ibm_job():
+    """Test retrieving a previously submitted IBM job."""
+    circuit = QuantumCircuit(1, 1)
+    circuit.h(0)
+    circuit.measure(0, 0)
+    qbraid_device = device_wrapper("ibm_q_qasm_simulator")
+    qbraid_job = qbraid_device.run(circuit, shots=1)
+    ibm_job = qbraid_job._get_vendor_jlo()
+    assert isinstance(ibm_job, IBMCircuitJob)
+
+
+@pytest.mark.skipif(skip_remote_tests, reason=REASON)
+def test_retrieving_ibm_job_raises_error():
+    """Test retrieving IBM job from unrecognized backend raises error."""
+    circuit = QuantumCircuit(1, 1)
+    circuit.h(0)
+    circuit.measure(0, 0)
+    qbraid_device = device_wrapper("ibm_q_qasm_simulator")
+    qbraid_job = qbraid_device.run(circuit, shots=1)
+    qbraid_job.device.vendor_dlo = Mock()
+    with pytest.raises(JobError):
+        qbraid_job._get_vendor_jlo()
