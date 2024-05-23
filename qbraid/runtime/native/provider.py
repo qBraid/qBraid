d37ac156ba0416492164746d93fbec0e184f7ad2
Ryan Hill <ryanjh88@gmail.com>
2024-05-23 12:10:38 -0500

    iqm + display devices

--- qbraid/runtime/native/provider.py
+++ qbraid/runtime/native/provider.py
@@ -36,11 +36,15 @@ class QbraidProvider(QuantumProvider):
         client (qbraid_core.services.quantum.QuantumClient): qBraid QuantumClient object
     """
 
-    def __init__(self, client: Optional[QuantumClient] = None):
+    def __init__(self, api_key: Optional[str] = None, client: Optional[QuantumClient] = None):
         """
         Initializes the QbraidProvider object
 
         """
+        if api_key and client:
+            raise ValueError("Provide either api_key or client, not both.")
+
+        self._api_key = None
         self._client = client
 
     def save_config(self, **kwargs):
@@ -52,7 +56,7 @@ class QbraidProvider(QuantumProvider):
         """Return the QuantumClient object."""
         if self._client is None:
             try:
-                self._client = QuantumClient()
+                self._client = QuantumClient(api_key=self._api_key)
             except AuthError as err:
                 raise ResourceNotFoundError(
                     "Failed to authenticate with the Quantum service."
@@ -138,13 +142,10 @@ class QbraidProvider(QuantumProvider):
         if tags:
             query.update({f"tags.{key}": value for key, value in tags.items()})
 
-        query_filter = query.copy()
-
         if max_results:
-            query_filter["numResults"] = max_results
             query["resultsPerPage"] = max_results
 
         jobs = self.client.search_jobs(query)
 
-        job_data, msg = process_job_data(jobs, query_filter)
+        job_data, msg = process_job_data(jobs, query)
         return display_jobs_from_data(job_data, msg)
