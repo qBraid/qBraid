85e557686b804cee6ad2391c14c71fddc4c1670c
dhanaabhirajk <dhanaabhirajk@gmail.com>
2024-06-11 08:39:17 +0530

    refactor: fix linting issues

--- qbraid/transforms/qasm3/transform.py
+++ qbraid/transforms/qasm3/transform.py
@@ -1,103 +1,113 @@
-"""Transform Program to program with basis gates."""
+# Copyright (C) 2024 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
 
-import numpy as np
-from openqasm3.ast import (
-    Program,
-    QuantumGate,
-    QuantumStatement,
-    Identifier,
-    FloatLiteral,
-    BinaryExpression,
-    BinaryOperator,
-    UnaryExpression,
-    UnaryOperator,
-)
+"""
+Module for providing transforamtions with basis gates.
+across various other quantum software frameworks.
+
+"""
+
 from typing import List
-from qbraid.transpiler.conversions.openqasm3 import openqasm3_to_qasm3
+
+from openqasm3 import ast
 from openqasm3.parser import parse
 
+from qbraid.transpiler.conversions.openqasm3 import openqasm3_to_qasm3
 
-def decompose_crx(gate: QuantumGate) -> List[QuantumStatement]:
+
+def decompose_crx(gate: ast.QuantumGate) -> List[ast.Statement]:
     """Decompose a crx gate into its basic gate equivalents."""
     theta = gate.arguments[0]
     control = gate.qubits[0]
     target = gate.qubits[1]
 
-    rz_pos_pi_half = QuantumGate(
+    rz_pos_pi_half = ast.QuantumGate(
         modifiers=[],
-        name=Identifier(name="rz"),
+        name=ast.Identifier(name="rz"),
         arguments=[
-            BinaryExpression(op=BinaryOperator(
-                17), lhs=Identifier(name="pi"), rhs=FloatLiteral(value=2))
+            ast.BinaryExpression(
+                op=ast.BinaryOperator(17),
+                lhs=ast.Identifier(name="pi"),
+                rhs=ast.FloatLiteral(value=2),
+            )
         ],
         qubits=[target],
     )
-    ry_pos_theta_half = QuantumGate(
+    ry_pos_theta_half = ast.QuantumGate(
         modifiers=[],
-        name=Identifier(name="ry"),
+        name=ast.Identifier(name="ry"),
         arguments=[
-            BinaryExpression(op=BinaryOperator(
-                17), lhs=theta, rhs=FloatLiteral(value=2))
+            ast.BinaryExpression(
+                op=ast.BinaryOperator(17), lhs=theta, rhs=ast.FloatLiteral(value=2)
+            )
         ],
         qubits=[target],
     )
-    cx = QuantumGate(
-        modifiers=[], name=Identifier(name="cx"),
-        arguments=[],
-        qubits=[control, target]
+    cx = ast.QuantumGate(
+        modifiers=[], name=ast.Identifier(name="cx"), arguments=[], qubits=[control, target]
     )
-    ry_neg_theta_half = QuantumGate(
+    ry_neg_theta_half = ast.QuantumGate(
         modifiers=[],
-        name=Identifier(name="ry"),
+        name=ast.Identifier(name="ry"),
         arguments=[
-            BinaryExpression(op=BinaryOperator(
-                17), lhs=UnaryExpression(UnaryOperator(3), theta), rhs=FloatLiteral(value=2))
+            ast.BinaryExpression(
+                op=ast.BinaryOperator(17),
+                lhs=ast.UnaryExpression(ast.UnaryOperator(3), theta),
+                rhs=ast.FloatLiteral(value=2),
+            )
         ],
         qubits=[target],
     )
-    rz_neg_pi_half = QuantumGate(
+    rz_neg_pi_half = ast.QuantumGate(
         modifiers=[],
-        name=Identifier(name="rz"),
+        name=ast.Identifier(name="rz"),
         arguments=[
-            BinaryExpression(op=BinaryOperator(
-                17), lhs=UnaryExpression(UnaryOperator(3), Identifier(name="pi")), rhs=FloatLiteral(value=2))
+            ast.BinaryExpression(
+                op=ast.BinaryOperator(17),
+                lhs=ast.UnaryExpression(ast.UnaryOperator(3), ast.Identifier(name="pi")),
+                rhs=ast.FloatLiteral(value=2),
+            )
         ],
         qubits=[target],
     )
     return [rz_pos_pi_half, ry_pos_theta_half, cx, ry_neg_theta_half, cx, rz_neg_pi_half]
 
 
-def decompose_cry(gate: QuantumGate) -> List[QuantumStatement]:
+def decompose_cry(gate: ast.QuantumGate) -> List[ast.Statement]:
     """Decompose a cry gate into its basic gate equivalents."""
     theta = gate.arguments[0]
     control = gate.qubits[0]
     target = gate.qubits[1]
 
-    ry_pos_theta_half = QuantumGate(
+    ry_pos_theta_half = ast.QuantumGate(
         modifiers=[],
-        name=Identifier(name="ry"),
+        name=ast.Identifier(name="ry"),
         arguments=[
-            BinaryExpression(op=BinaryOperator(
-                17), lhs=theta, rhs=FloatLiteral(value=2))
+            ast.BinaryExpression(
+                op=ast.BinaryOperator(17), lhs=theta, rhs=ast.FloatLiteral(value=2)
+            )
         ],
         qubits=[target],
     )
-    cx = QuantumGate(
-        modifiers=[], name=Identifier(name="cx"),
-        arguments=[],
-        qubits=[control, target]
+    cx = ast.QuantumGate(
+        modifiers=[], name=ast.Identifier(name="cx"), arguments=[], qubits=[control, target]
     )
-    ry_neg_theta_half = QuantumGate(
+    ry_neg_theta_half = ast.QuantumGate(
         modifiers=[],
-        name=Identifier(name="ry"),
+        name=ast.Identifier(name="ry"),
         arguments=[
-            UnaryExpression(
-                UnaryOperator(3),
-                BinaryExpression(
-                    op=BinaryOperator(17),
-                    lhs=theta,
-                    rhs=FloatLiteral(value=2)
-                )
+            ast.UnaryExpression(
+                ast.UnaryOperator(3),
+                ast.BinaryExpression(
+                    op=ast.BinaryOperator(17), lhs=theta, rhs=ast.FloatLiteral(value=2)
+                ),
             )
         ],
         qubits=[target],
@@ -105,37 +115,34 @@ def decompose_cry(gate: QuantumGate) -> List[QuantumStatement]:
     return [ry_pos_theta_half, cx, ry_neg_theta_half, cx]
 
 
-def decompose_crz(gate: QuantumGate) -> List[QuantumStatement]:
+def decompose_crz(gate: ast.QuantumGate) -> List[ast.Statement]:
     """Decompose a cry gate into its basic gate equivalents."""
     theta = gate.arguments[0]
     control = gate.qubits[0]
     target = gate.qubits[1]
 
-    rz_pos_theta_half = QuantumGate(
+    rz_pos_theta_half = ast.QuantumGate(
         modifiers=[],
-        name=Identifier(name="rz"),
+        name=ast.Identifier(name="rz"),
         arguments=[
-            BinaryExpression(op=BinaryOperator(
-                17), lhs=theta, rhs=FloatLiteral(value=2))
+            ast.BinaryExpression(
+                op=ast.BinaryOperator(17), lhs=theta, rhs=ast.FloatLiteral(value=2)
+            )
         ],
         qubits=[target],
     )
-    cx = QuantumGate(
-        modifiers=[], name=Identifier(name="cx"),
-        arguments=[],
-        qubits=[control, target]
+    cx = ast.QuantumGate(
+        modifiers=[], name=ast.Identifier(name="cx"), arguments=[], qubits=[control, target]
     )
-    rz_neg_theta_half = QuantumGate(
+    rz_neg_theta_half = ast.QuantumGate(
         modifiers=[],
-        name=Identifier(name="rz"),
+        name=ast.Identifier(name="rz"),
         arguments=[
-            UnaryExpression(
-                UnaryOperator(3),
-                BinaryExpression(
-                    op=BinaryOperator(17),
-                    lhs=theta,
-                    rhs=FloatLiteral(value=2)
-                )
+            ast.UnaryExpression(
+                ast.UnaryOperator(3),
+                ast.BinaryExpression(
+                    op=ast.BinaryOperator(17), lhs=theta, rhs=ast.FloatLiteral(value=2)
+                ),
             )
         ],
         qubits=[target],
@@ -143,30 +150,41 @@ def decompose_crz(gate: QuantumGate) -> List[QuantumStatement]:
     return [rz_pos_theta_half, cx, rz_neg_theta_half, cx]
 
 
-def decompose_cy(gate: QuantumGate) -> List[QuantumStatement]:
+def decompose_cy(gate: ast.QuantumGate) -> List[ast.Statement]:
     """Decompose a cy gate into its basic gate equivalents."""
     control = gate.qubits[0]
     target = gate.qubits[1]
 
-    cry_pi = QuantumGate(
+    cry_pi = ast.QuantumGate(
         modifiers=[],
-        name=Identifier(name="cry"),
-        arguments=[Identifier(name="pi")],
+        name=ast.Identifier(name="cry"),
+        arguments=[ast.Identifier(name="pi")],
         qubits=[control, target],
     )
-    s = QuantumGate(
-        modifiers=[], name=Identifier(name="s"),
-        arguments=[],
-        qubits=[control]
+    s = ast.QuantumGate(modifiers=[], name=ast.Identifier(name="s"), arguments=[], qubits=[control])
+    return transform_program(ast.Program(statements=[cry_pi, s])).statements
+
+
+def decompose_cz(gate: ast.QuantumGate) -> List[ast.Statement]:
+    """Decompose a cz gate into its basic gate equivalents."""
+    control = gate.qubits[0]
+    target = gate.qubits[1]
+
+    crz_pi = ast.QuantumGate(
+        modifiers=[],
+        name=ast.Identifier(name="crz"),
+        arguments=[ast.Identifier(name="pi")],
+        qubits=[control, target],
     )
-    return transform_program(Program(statements=[cry_pi, s])).statements
+    s = ast.QuantumGate(modifiers=[], name=ast.Identifier(name="s"), arguments=[], qubits=[control])
+    return transform_program(ast.Program(statements=[crz_pi, s])).statements
 
 
-def transform_program(program: Program) -> Program:
+def transform_program(program: ast.Program) -> ast.Program:
     """Transform a QASM program, decomposing crx gates."""
     transformed_statements = []
     for statement in program.statements:
-        if isinstance(statement, QuantumGate):
+        if isinstance(statement, ast.QuantumGate):
             if statement.name.name == "crx":
                 transformed_statements.extend(decompose_crx(statement))
             elif statement.name.name == "cry":
@@ -175,17 +193,20 @@ def transform_program(program: Program) -> Program:
                 transformed_statements.extend(decompose_crz(statement))
             elif statement.name.name == "cy":
                 transformed_statements.extend(decompose_cy(statement))
+            elif statement.name.name == "cz":
+                transformed_statements.extend(decompose_cz(statement))
             else:
                 transformed_statements.append(statement)
         else:
             transformed_statements.append(statement)
 
-    return Program(statements=transformed_statements, version=program.version)
+    return ast.Program(statements=transformed_statements, version=program.version)
 
 
 def convert_to_basis_gates(qasm: str, basis_gates: list[str]) -> str:
     """
-    Converts an OpenQASM 3 program to an equivalent program that only uses the specified basis gates.
+    Converts an OpenQASM 3 program to an equivalent program
+    Only uses the specified basis gates.
 
     Args:
         openqasm_program (str): The original OpenQASM 3 program as a string.
@@ -197,6 +218,7 @@ def convert_to_basis_gates(qasm: str, basis_gates: list[str]) -> str:
     Raises:
         ValueError: if the decomposition is not possible
     """
+    print(basis_gates)
 
     program = parse(qasm)
 
--- tests/transforms/test_qasm3_transforms.py
+++ tests/transforms/test_qasm3_transforms.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2024 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+Unit tests for QASM transform to basic gates
+
+"""
+
 import pytest
 
 from qbraid.transforms.qasm3.transform import convert_to_basis_gates
@@ -6,30 +21,14 @@ from qbraid.transforms.qasm3.transform import convert_to_basis_gates
 @pytest.mark.parametrize(
     "original_program, expected_program",
     [
-        # (
-        #     """
-        #     OPENQASM 3;
-        #     include "stdgates.inc";
-        #     qubit[2] q;
-        #     cz q[0], q[1];
-        #     """,
-        #     """
-        #     OPENQASM 3;
-        #     include "stdgates.inc";
-        #     qubit[2] q;
-        #     h q[1];
-        #     cx q[0], q[1];
-        #     h q[1];
-        #     """,
-        # ),
-(
-"""
+        (
+            """
 OPENQASM 3.0;
 include "stdgates.inc";
 qubit[2] q;
 crx(pi) q[0], q[1];
 """,
-"""OPENQASM 3.0;
+            """OPENQASM 3.0;
 include "stdgates.inc";
 qubit[2] q;
 rz(pi / 2) q[1];
@@ -39,45 +38,45 @@ ry(-pi / 2) q[1];
 cx q[0], q[1];
 rz(-pi / 2) q[1];
 """,
-),
-(
-"""
+        ),
+        (
+            """
 OPENQASM 3.0;
 include "stdgates.inc";
 qubit[2] q;
 cry(pi/4) q[0], q[1];
 """,
-"""OPENQASM 3.0;
+            """OPENQASM 3.0;
 include "stdgates.inc";
 qubit[2] q;
 ry(pi / 4 / 2) q[1];
 cx q[0], q[1];
 ry(-(pi / 4 / 2)) q[1];
 cx q[0], q[1];
-"""
-),
-(
-"""
+""",
+        ),
+        (
+            """
 OPENQASM 3.0;
 include "stdgates.inc";
 qubit[2] q;
 crz(pi/4) q[0], q[1];
 """,
-"""OPENQASM 3.0;
+            """OPENQASM 3.0;
 include "stdgates.inc";
 qubit[2] q;
 rz(pi / 4 / 2) q[1];
 cx q[0], q[1];
 rz(-(pi / 4 / 2)) q[1];
 cx q[0], q[1];
-"""
-),
-(
-"""OPENQASM 3.0;
+""",
+        ),
+        (
+            """OPENQASM 3.0;
 include "stdgates.inc";
 qubit[2] q;
 cy q[0], q[1];""",
-"""OPENQASM 3.0;
+            """OPENQASM 3.0;
 include "stdgates.inc";
 qubit[2] q;
 ry(pi / 2) q[1];
@@ -85,13 +84,27 @@ cx q[0], q[1];
 ry(-(pi / 2)) q[1];
 cx q[0], q[1];
 s q[0];
-"""
-)
-
-    ]
+""",
+        ),
+        (
+            """OPENQASM 3.0;
+include "stdgates.inc";
+qubit[2] q;
+cz q[0], q[1];""",
+            """OPENQASM 3.0;
+include "stdgates.inc";
+qubit[2] q;
+rz(pi / 2) q[1];
+cx q[0], q[1];
+rz(-(pi / 2)) q[1];
+cx q[0], q[1];
+s q[0];
+""",
+        ),
+    ],
 )
 def test_convert_to_basis_gates(original_program, expected_program):
-
+    """Test conversion of QASM3 program to basis gates"""
     basis_gates = [
         "x",
         "y",
