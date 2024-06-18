10c121d72b0a2b3c0cd7dcee61ca2042226877d8
Pranet Sharma <44727373+pranetksharma@users.noreply.github.com>
2024-06-18 11:09:05 -0500

    Update test_azure_provider.py
    completed tests, if any formatting needs to be changed let me know

--- tests/runtime/test_azure_provider.py
+++ tests/runtime/test_azure_provider.py
@@ -13,53 +13,117 @@ Unit tests for Azure session, provider, and device classes
 
 """
 import pytest
-
 from qbraid.runtime.azure import AzureQuantumDevice, AzureQuantumProvider, AzureSession
 
+provider = AzureQuantumProvider(client_id=client_id, client_secret=client_secret, tenant_id=tenant_id,
+                                location_name=location_name, subscription_id=subscription_id, resource_group_name=resource_group,
+                                workspace_name=workspace_name, storage_account=storage_account, connection_string=connection_string)
 
-@pytest.mark.skip(reason="Not implemented")
+session = provider.session
+
+@pytest.fixture
 def test_session_get_devices():
     """Test getting data for all Azure Quantum devices."""
-    raise NotImplementedError
+    check_devices = ['microsoft.estimator', 'quantinuum.qpu.h1-1', 'quantinuum.sim.h1-1sc', 'quantinuum.sim.h1-1e', 'rigetti.sim.qvm', 'rigetti.qpu.ankaa-2']
+    all_devices = []
+
+    devices = session.get_devices()
+    for device, data in devices.items():
+        all_devices.append(device)
+
+    assert all_devices == check_devices
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_session_get_device():
     """Getting data for specific Azure Quantum device."""
-    raise NotImplementedError
+    check_parameters = ['status', 'isAvailable', 'nextAvailable', 'availablilityCD', 'averageQueueTime']
+    device_parameters = []
 
+    device = session.get_device("microsoft.estimator")
+    for parameter, status in device.items():
+        device_parameters.append(parameter)
 
-@pytest.mark.skip(reason="Not implemented")
+    assert device_parameters == check_parameters
+
 def test_session_create_job():
     """Test creating a new job through the Azure Quantum API."""
-    raise NotImplementedError
 
+    check_parameters = ['containerUri', 'inputDataUri', 'inputDataFormat', 
+                        'inputParams', 'metadata', 'sessionId', 'status', 
+                        'jobType', 'outputDataFormat', 'outputDataUri', 
+                        'beginExecutionTime', 'cancellationTime', 'quantumComputingData', 
+                        'errorData', 'isCancelling', 'tags', 'name', 
+                        'id', 'providerId', 'target', 'creationTime', 
+                        'endExecutionTime', 'costEstimate', 'itemType']
+    
+    job_parameters = []
 
-@pytest.mark.skip(reason="Not implemented")
+    job = session.create_job("job-data-test-cases", "quantinuum", "Emulator", 2)
+
+    for parameter, status in job.items():
+            job_parameters.append(parameter)
+
+    assert job_parameters == check_parameters
+
 def test_session_get_job():
     """Teset getting a specific Azure Quantum job."""
-    raise NotImplementedError
+    check_parameters = ['containerUri', 'inputDataUri', 'inputDataFormat', 
+                        'inputParams', 'metadata', 'sessionId', 'status', 
+                        'jobType', 'outputDataFormat', 'outputDataUri', 
+                        'beginExecutionTime', 'cancellationTime', 'quantumComputingData', 
+                        'errorData', 'isCancelling', 'tags', 'name', 
+                        'id', 'providerId', 'target', 'creationTime', 
+                        'endExecutionTime', 'costEstimate', 'itemType']
+    
+    job_parameters = []
 
+    session.create_job("job-data-test-cases", "quantinuum", "Emulator", 2)
 
-@pytest.mark.skip(reason="Not implemented")
+    for key, value in session.jobs.items():
+        job_id = key
+        break
+
+    get_job = session.get_job(job_id)
+
+    for parameter, status in get_job.items():
+            job_parameters.append(parameter)
+
+    assert job_parameters == check_parameters
+
 def test_session_cancel_job():
     """Test cancelling a specific Azure Quantum job."""
-    raise NotImplementedError
+    session.create_job("job-data-test-cases", "quantinuum", "Emulator", 2)
 
+    for key, value in session.jobs.items():
+        job_id = key
+        break
 
-@pytest.mark.skip(reason="Not implemented")
+    session.cancel_job(job_id)
+    metadata = session.get_job(job_id)
+
+    assert metadata["isCancelling"] == True
+    
+@pytest.fixture
 def test_provider_get_devices():
     """Test getting list of all AzureQuantumDevice objects."""
-    raise NotImplementedError
+    devices = provider.get_devices()
+    assert len(devices) >= 1
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_provider_get_device():
     """Test getting a specific AzureQuantumDevice object."""
-    raise NotImplementedError
+    check_devices = ['id', 'status', 'isAvailable', 'nextAvailable', 'availablilityCD', 'averageQueueTime']
+    verify_devices = []
 
+    devices = provider.get_device("microsoft.estimator")
 
-@pytest.mark.skip(reason="Not implemented")
+    for parameter, status in devices.items():
+        verify_devices.append(parameter)
+
+    assert check_devices == verify_devices
+    
 def test_device_status():
     """Test getting status of AzureQuantumDevice."""
-    raise NotImplementedError
+    devices = provider.get_device("microsoft.estimator")
+    assert "status" in devices
+    
