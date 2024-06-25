834e495e5e98d6f21a76526030e0451fbcf595c2
Rohan Jain <rohanjain@cmu.edu>
2024-06-25 15:06:35 -0500

    more codecov (#690)
    * added cirq->braket support for ionq gates
    
    * updated ionq gate support
    
    * format
    
    * oops
    
    * isort
    
    * black
    
    * add back ms gate to test
    
    * fix ms
    
    * black formatting
    
    * circuits allclose codecov
    
    * random qasm3 codecov
    
    * random circuit codecov
    
    * formatting
    
    * plot graph codecov
    
    * qasm3 drawer codecov
    
    * fixed bad function get_circuit_height
    
    * qasm2 decompose codecov
    
    * qasm3 pass codecov
    
    * remove useless line
    
    * more codecov
    
    * more codecov
    
    * qasm3 drawer codecov
    
    * pylint
    
    * codecov
    
    * pylint
    
    * codecov
    
    * black formatting
    
    * fix test error
    
    * whitesapce

---
+++ tests/passes/test_qasm2_decompose.py
@@ -0,0 +1,67 @@
+# Copyright (C) 2024 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+Unit tests for QASM transform to basic gates
+
+"""
+
+import pytest
+
+from qbraid.passes.exceptions import QasmDecompositionError
+from qbraid.passes.qasm2.decompose import decompose_qasm_qelib1
+
+cu = """
+OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[2];
+cu(0.1,0.2,0.3) q[0],q[1];
+"""
+
+rxx = """
+OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[2];"
+rxx( q[0];
+"""
+
+rccx = """
+OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[3];
+rccx q[0],q[1];
+"""
+
+rc3x = """
+OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[4];
+rc3x q[0],q[1],q[2];
+"""
+
+
+@pytest.mark.parametrize("program", [cu, rxx, rccx, rc3x])
+def test_fail_decompose_instr(program):
+    """Test invalid instruction decomposition"""
+    with pytest.raises(QasmDecompositionError):
+        decompose_qasm_qelib1(program)
+
+
+rxx_none = """
+OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[2];"
+rxx( q[0], q[1];
+"""
+
+
+def test_decompose_none():
+    """Test no parameters in function decomposition."""
+    assert "rz(None)" in decompose_qasm_qelib1(rxx_none)
