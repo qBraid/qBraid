c4cf2a7a9d74b0b2b45ee483b0e22f3e614b0192
Pranet Sharma <44727373+pranetksharma@users.noreply.github.com>
2024-07-23 13:33:20 -0400

    Create __init__.py for Azure

---
+++ qbraid/runtime/azure/__init__.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2024 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+Mdule submiting and managing jobs through the Azure Quantum API.
+
+.. currentmodule:: qbraid.runtime.azure
+
+Classes
+--------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+	   AzureSession
+	   AzureQuantumProvider
+      AzureQuantumDevice
+      AzureQuantumJob
+
+"""
+from .device import AzureQuantumDevice
+from .provider import AzureQuantumProvider
+from .session import AzureSession
+from .job import AzureJob
+
+__all__ = ["AzureSession", "AzureQuantumProvider", "AzureQuantumDevice", "AzureJob"]
