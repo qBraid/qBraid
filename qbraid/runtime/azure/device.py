ac8eae9e21e6b414de141b8dfdbeca6e1af4fb05
Pranet Sharma <44727373+pranetksharma@users.noreply.github.com>
2024-07-26 17:03:41 -0400

    Update device.py with Azure credential changes

--- qbraid/runtime/azure/device.py
+++ qbraid/runtime/azure/device.py
@@ -13,9 +13,8 @@ Module defining Azure Quantum device class for all devices managed by Azure Quan
 
 """
 
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Any
 
-from qbraid.programs import load_program
 from qbraid.runtime.device import QuantumDevice
 from qbraid.runtime.enums import DeviceStatus
 
@@ -43,9 +42,12 @@ class AzureQuantumDevice(QuantumDevice):
         return self._client
 
     def status(self) -> "qbraid.runtime.enums.DeviceStatus":
-        """Return the current status of the Azure device."""
+        """Return the current status of the Azure device.
+
+        Returns: DeviceStatus: The current status of the device.
+        """
         device_data = self.client.get_device(self.id)
-        status = device_data["status"]
+        status = device_data["_current_availability"]
         status_map = {
             "Available": DeviceStatus.ONLINE,
             "Deprecated": DeviceStatus.UNAVAILABLE,
@@ -53,86 +55,25 @@ class AzureQuantumDevice(QuantumDevice):
         }
         return status_map.get(status, DeviceStatus.UNAVAILABLE)
 
-    def submit(
-        self, run_input: str, *args, shots: int = 100, name: Optional[str] = None, **kwargs
-    ) -> AzureJob:
-        """Submit a job to the Azure device."""
+    # pylint:disable=arguments-differ
+    def submit(self, program: Any, job_name: str, shots: int = 100) -> AzureJob:
+        """Submit a job to the Azure device.
 
-        if isinstance(run_input, list):
+        Args: program (Any): The program to submit.
+              job_name (str): The name of the job.
+              shots (int): The number of shots to run.
+
+        Returns: AzureJob: The submitted job.
+        """
+
+        if isinstance(program, list):
             raise ValueError(
                 "Batch jobs (list of inputs) are not supported for this device. "
                 "Please provide a single job input."
             )
 
-        provider = self.profile.get("provider_name")
-        storage_data = self.client.create_job_storage(run_input, "application/qasm")
-        name = name or f"{provider}-job"
-
-        job_id = storage_data["id"]
-        container_sas_uri = storage_data["containerUri"]
-        input_sas_uri = storage_data["inputDataUri"]
-        output_sas_uri = storage_data["outputDataUri"]
-
-        formats = {"quantinuum": ["honeywell.openqasm.v1", "honeywell.quantum-results.v1"]}
-        input_data_format, output_data_format = formats[provider]
-
-        program = load_program(run_input)
-        num_qubits = program.num_qubits
-
-        payload = self.create_payload(
-            container_sas_uri,
-            input_sas_uri,
-            output_sas_uri,
-            job_id,
-            job_name=name,
-            input_data_format=input_data_format,
-            provider=provider,
-            backend=self.id,
-            num_qubits=num_qubits,
-            num_shots=shots,
-            total_count=100,
-            output_data_format=output_data_format,
+        job = self.client.submit_job(
+            {"device_name": self.id, "program": program, "job_name": job_name, "shots": shots}
         )
 
-        self.client.submit_job(payload)
-
-        return AzureJob(job_id=job_id, client=self.client, device=self)
-
-    @staticmethod
-    def create_payload(  # pylint: disable=too-many-arguments
-        container_uri: str,
-        input_uri: str,
-        output_uri: str,
-        job_id: str,
-        job_name: str,
-        input_data_format: str,
-        provider: str,
-        backend: str,
-        num_qubits: str,
-        num_shots: int,
-        total_count: int,
-        output_data_format: str,
-    ) -> dict:
-        """Create the payload for the job."""
-        payload = {
-            "containerUri": container_uri,
-            "id": job_id,
-            "inputDataFormat": input_data_format,  # "honeywell.openqasm.v1"
-            "itemType": "Job",
-            "name": job_name,
-            "providerId": provider,
-            "target": backend,  # changeable
-            "metadata": {
-                "qiskit": "True",
-                "name": job_name,
-                "num_qubits": num_qubits,
-                "metadata": "null",
-                "meas_map": "[0]",
-            },
-            "inputDataUri": input_uri,
-            "inputParams": {"shots": num_shots, "count": total_count},
-            "outputDataFormat": output_data_format,  # "honeywell.quantum-results.v1"
-            "outputDataUri": output_uri,
-        }
-
-        return payload
+        return AzureJob(job_id=job.id, client=self.client, device=self)
