e12c3b77a42becc3afd0b180516ebb94db43ed00
Pranet Sharma <44727373+pranetksharma@users.noreply.github.com>
2024-07-30 17:44:27 -0400

    fixed typing issues

--- qbraid/runtime/azure/result.py
+++ qbraid/runtime/azure/result.py
@@ -7,11 +7,18 @@
 # See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
 #
 # THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+#
+# pylint: disable=arguments-differ
+
 
 """
 Module defining Azure Results class
 
 """
+
+from collections import Counter
+
+import numpy as np
 
 from qbraid.runtime.result import GateModelJobResult
 
@@ -19,9 +26,16 @@ from qbraid.runtime.result import GateModelJobResult
 class AzureResult(GateModelJobResult):
     """Azure result class."""
 
+    def __init__(self, result: dict):
+        super().__init__(result=result)
+        self._result = result
+
     def measurements(self):
         """Return the measurements from the result data."""
-        return self._result.keys()
+        return np.array(self._result["meas"])
+
+    def measurement_counts(self):
+        return dict(Counter(self.measurements()))
 
     def raw_counts(self, **kwargs):
         """Return the raw counts from the result data."""
