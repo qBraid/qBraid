f0fdf556ae3c7b2eaa6fd17fb4fbe98a9f13cbbd
Ryan Hill <ryanjh88@gmail.com>
2024-10-04 21:35:15 -0500

    annealing program types

---
+++ qbraid/programs/annealing/__init__.py
@@ -0,0 +1,58 @@
+# Copyright (C) 2024 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+Module containing sub-modules for interfacing with
+various quantum software libraries and program types.
+
+.. currentmodule:: qbraid.programs.annealing
+
+Classes
+--------
+
+.. autosummary::
+   :toctree: ../stubs/
+    
+    ProblemType
+    Problem
+    AnnealingProblem
+
+Submodules
+------------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+    pyqubo
+
+"""
+import importlib
+
+from ._model import AnnealingProblem, Problem, ProblemType
+
+_qbraid = importlib.import_module("qbraid.programs._import")
+NATIVE_REGISTRY = getattr(_qbraid, "NATIVE_REGISTRY", {})
+
+submodules = []
+base_path = "qbraid.programs.annealing."
+
+for lib in NATIVE_REGISTRY:
+    try:
+        imported_lib = importlib.import_module(base_path + lib)
+        submodules.append(lib)
+        globals()[lib] = imported_lib
+
+    except ImportError:
+        pass
+
+
+__all__ = ["ProblemType", "Problem", "AnnealingProblem"]
+
+__all__.extend(submodules)
---
+++ qbraid/programs/annealing/pyqubo.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2024 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+Module defining PyQuboModel Class
+
+"""
+from __future__ import annotations
+
+from cpp_pyqubo import Model
+
+from qbraid.programs.exceptions import ProgramTypeError
+
+from ._model import AnnealingProblem, Problem, ProblemType, QuboProblem
+
+
+class PyQuboModel(AnnealingProblem):
+    """AnnealingProblem subclass that accepts a cpp_pyqubo.Model."""
+
+    def __init__(self, program: Model):
+        super().__init__(program)
+        if not isinstance(program, Model):
+            raise ProgramTypeError(
+                message=f"Expected 'cpp_pyqubo.Model' object, got '{type(program)}'."
+            )
+        self.problem_type = ProblemType.QUBO
+
+    def to_problem(self) -> Problem:
+        """Converts the cpp_pyqubo.Model to a Problem instance."""
+        qubo, offset = self.program.to_qubo()
+        coefficients = {}
+
+        for key, value in qubo.items():
+            coefficients[key] = value
+
+        return QuboProblem(coefficients, offset)
