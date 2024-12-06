22eb0b9f45692ba73c04551274160b68e1db0daf
Alvan Caleb Arulandu <calebarulandu@gmail.com>
2024-12-06 14:09:24 -0500

    qasm -> cudaq config

---
+++ qbraid/transpiler/conversions/openqasm3/openqasm3_to_cudaq.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2024 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+Module containing OpenQASM to CUDA-Q conversion function
+
+"""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import openqasm3
+import cudaq
+
+from qbraid.transpiler.annotations import weight
+
+@weight(1)
+def openqasm3_to_cudaq(program: openqasm3.ast.Program):
+    kernel = cudaq.make_kernel()
+    return kernel
