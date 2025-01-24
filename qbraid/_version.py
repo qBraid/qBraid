79e744d514b7e1cd468c0fa6756aef92faf9000f
Ryan Hill <ryanjh88@gmail.com>
2025-01-24 12:49:27 -0600

    account for rustworkx upgrade

--- qbraid/_version.py
+++ qbraid/_version.py
@@ -15,4 +15,4 @@ Version number (major.minor.patch[-label])
 
 """
 
-__version__ = "0.9.2"
+__version__ = "0.9.3.dev"
--- qbraid/visualization/plot_conversions.py
+++ qbraid/visualization/plot_conversions.py
@@ -133,12 +133,12 @@ def plot_conversion_graph(  # pylint: disable=too-many-arguments
     ]
 
     rustworkx_version = rx.__version__  # pylint: disable=no-member
-    if len(set(ecolors)) > 1 and rustworkx_version in ["0.15.0", "0.15.1"]:
+    if len(set(ecolors)) > 1 and rustworkx_version in ["0.15.0", "0.15.1", "0.16.0"]:
         warnings.warn(
             "Detected multiple edge colors, which may not display correctly "
-            "due to a known bug in rustworkx versions 0.15.0 and 0.15.1 "
+            "due to a known bug in rustworkx>=0.15.0,<=0.16.0 "
             "(see: https://github.com/Qiskit/rustworkx/issues/1308). "
-            "To avoid this issue, please upgrade to rustworkx>0.15.1.",
+            "To avoid this issue, please upgrade to rustworkx>0.16.0.",
             UserWarning,
         )
 
