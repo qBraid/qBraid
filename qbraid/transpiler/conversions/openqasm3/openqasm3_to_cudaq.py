d055f0827c11f75a7d608ed2242e9fbcca26557d
Ryan Hill <ryanjh88@gmail.com>
2025-02-10 19:01:41 -0600

    weight and recursion error catch

--- qbraid/transpiler/conversions/openqasm3/openqasm3_to_cudaq.py
+++ qbraid/transpiler/conversions/openqasm3/openqasm3_to_cudaq.py
@@ -20,7 +20,7 @@ import pyqasm
 from openqasm3 import ast
 from qbraid_core._import import LazyLoader
 
-from qbraid.transpiler.annotations import requires_extras, weight
+from qbraid.transpiler.annotations import weight
 from qbraid.transpiler.exceptions import ProgramConversionError
 
 cudaq = LazyLoader("cudaq", globals(), "cudaq")
@@ -73,9 +73,7 @@ def make_gate_kernel(name: str, targs: tuple[type]) -> PyKernel:
     return kernel
 
 
-@weight(1)
-@requires_extras("cudaq")
-# pylint: disable-next=too-many-statements
+@weight(1)  # pylint: disable-next=too-many-statements
 def openqasm3_to_cudaq(program: QasmStringType | ast.Program) -> PyKernel:
     """Returns a CUDA-Q kernel representing the input OpenQASM program.
 
