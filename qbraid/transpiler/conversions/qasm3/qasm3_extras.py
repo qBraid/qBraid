44c77e0bb9fc88b15649c9d956f28496bc07371b
LukeAndreesen <107073823+LukeAndreesen@users.noreply.github.com>
2025-07-16 17:41:23 -0500

    autoqasm-qasm3 conversion extra (#1014)
    * initial commit - autoqasm -> qasm3 conversion extra
    
    * update changelog
    
    * resolve changelog diff
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    * Update requirements-dev.txt
    
    Co-authored-by: Ryan Hill <ryanjh88@gmail.com>
    
    * lazy loading, more robust testing, fixed autoqasm -> qasm gate conversion issues
    
    * implemented standard gate set conversion autoqasm -> qasm3
    
    * add autoqasm to autodoc mock imports]
    
    * docstring fix
    
    * cleanup autoqasm conversion
    
    * update insert_gate_def to add multiple defs in single pass
    
    * format
    
    * fix changelog
    
    * move gate defn. insertion to after include statement, codecov
    
    * missed docstring
    
    * removed unnecessary import
    
    * Allow QASM3 gate definition before inclusion statement
    
    * testing cov
    
    * added test for code cov
    
    * code coverage
    
    * removed redundant gate defn. logic
    
    * rm pylint disable in test conversion extra

--- qbraid/transpiler/conversions/qasm3/qasm3_extras.py
+++ qbraid/transpiler/conversions/qasm3/qasm3_extras.py
@@ -18,9 +18,27 @@ from typing import TYPE_CHECKING
 
 from qbraid_core._import import LazyLoader
 
+from qbraid.passes.qasm.compat import add_stdgates_include, insert_gate_def, replace_gate_names
 from qbraid.transpiler.annotations import requires_extras
 
 qbraid_qir = LazyLoader("qbraid_qir", globals(), "qbraid_qir")
+autoqasm = LazyLoader("autoqasm", globals(), "autoqasm")
+
+if TYPE_CHECKING:
+    import autoqasm as aq
+
+aq_to_qasm3_stdgates = {
+    "ccnot": "ccx",
+    "cnot": "cx",
+    "cphaseshift": "cp",
+    "i": "id",
+    "phaseshift": "p",
+    "si": "sdg",
+    "ti": "tdg",
+    "v": "sx",
+    "vi": "sxdg",
+}
+
 
 if TYPE_CHECKING:
     import pyqir
@@ -39,3 +57,26 @@ def qasm3_to_pyqir(program: Qasm3StringType) -> pyqir.Module:
         pyqir.Module: module equivalent to input OpenQASM 3 program.
     """
     return qbraid_qir.qasm3.qasm3_to_qir(program)
+
+
+@requires_extras("autoqasm")
+def autoqasm_to_qasm3(program: aq.program.program.Program) -> Qasm3StringType:
+    """Converts an AutoQASM program to an OpenQASM 3 program of gates from the
+    Standard Gate Library. The program must be built prior to conversion using
+    the .build() method.
+
+    Args:
+        program (autoqasm.program.program.Program): AutoQASM program to convert
+            to OpenQASM 3.
+
+    Returns:
+        str: OpenQASM 3 program equivalent to input AutoQASM program.
+    """
+    qasm = program.to_ir()
+    # Convert to Standard Library qasm3 gates
+    qasm = replace_gate_names(qasm, aq_to_qasm3_stdgates)
+    # Insert custom gate conversions
+    qasm = insert_gate_def(qasm, ["iswap", "sxdg", "cv"])
+    # AutoQASM does not include stdgates.inc
+    qasm = add_stdgates_include(qasm)
+    return qasm
--- tests/transpiler/test_conversion_extras.py
+++ tests/transpiler/test_conversion_extras.py
@@ -25,6 +25,9 @@ try:
 except ImportError:
     pyqir_installed = False
 
+
+from qbraid.passes.qasm.compat import normalize_qasm_gate_params
+from qbraid.transpiler.conversions.qasm3 import autoqasm_to_qasm3
 from qbraid.transpiler.conversions.qiskit import qiskit_to_braket, qiskit_to_pyqir
 from qbraid.transpiler.converter import transpile
 from qbraid.transpiler.edge import Conversion
@@ -66,3 +69,94 @@ def test_qiskit_to_pyqir_extra(bell_circuit):
     graph = ConversionGraph(conversions)
     program = transpile(qiskit_circuit, "pyqir", conversion_graph=graph, max_path_depth=1)
     assert isinstance(program, pyqir.Module)
+
+
+def autoqasm_bell_circuit():
+    """Function that returns autoqasm bell circuit."""
+    # pylint: disable-next=import-outside-toplevel
+    from ..fixtures.autoqasm.circuits import autoqasm_bell
+
+    return autoqasm_bell()
+
+
+def qasm3_bell_reference():
+    """Reference QASM3 string for Bell circuit"""
+    return """OPENQASM 3.0;
+include "stdgates.inc";
+qubit[2] __qubits__;
+h __qubits__[0];
+cx __qubits__[0], __qubits__[1];"""
+
+
+@pytest.mark.skipif(not has_extra(autoqasm_to_qasm3), reason="Extra not installed")
+def test_autoqasm_bell_to_qasm3_extra():
+    """Test autoqasm-qasm3 conversion extra."""
+    autoqasm_circuit = autoqasm_bell_circuit()
+    conversions = [Conversion("autoqasm", "qasm3", autoqasm_to_qasm3)]
+    graph = ConversionGraph(conversions)
+    program = transpile(autoqasm_circuit, "qasm3", conversion_graph=graph, max_path_depth=1)
+    assert isinstance(program, str)
+    assert program == qasm3_bell_reference()
+
+
+def autoqasm_shared15_circuit():
+    """Function that returns autoqasm shared15 circuit."""
+    # pylint: disable-next=import-outside-toplevel
+    from ..fixtures.autoqasm.circuits import autoqasm_shared15
+
+    return autoqasm_shared15()
+
+
+def qasm3_shared15_reference():
+    """Reference QASM3 string for shared15 circuit"""
+    return """OPENQASM 3.0;
+include "stdgates.inc";
+gate sxdg _gate_q_0 {
+  s _gate_q_0;
+  h _gate_q_0;
+  s _gate_q_0;
+}
+gate iswap _gate_q_0, _gate_q_1 {
+  s _gate_q_0;
+  s _gate_q_1;
+  h _gate_q_0;
+  cx _gate_q_0, _gate_q_1;
+  cx _gate_q_1, _gate_q_0;
+  h _gate_q_1;
+}
+qubit[4] __qubits__;
+h __qubits__[0];
+h __qubits__[1];
+h __qubits__[2];
+h __qubits__[3];
+x __qubits__[0];
+x __qubits__[1];
+y __qubits__[2];
+z __qubits__[3];
+s __qubits__[0];
+sdg __qubits__[1];
+t __qubits__[2];
+tdg __qubits__[3];
+rx(pi/4) __qubits__[0];
+ry(pi/2) __qubits__[1];
+rz(3*pi/4) __qubits__[2];
+p(pi/8) __qubits__[3];
+sx __qubits__[0];
+sxdg __qubits__[1];
+iswap __qubits__[2], __qubits__[3];
+swap __qubits__[0], __qubits__[2];
+swap __qubits__[1], __qubits__[3];
+cx __qubits__[0], __qubits__[1];
+cp(pi/4) __qubits__[2], __qubits__[3];"""
+
+
+@pytest.mark.skipif(not has_extra(autoqasm_to_qasm3), reason="Extra not installed")
+def test_autoqasm_shared15_to_qasm3_extra():
+    """Test autoqasm-qasm3 conversion extra."""
+    autoqasm_circuit = autoqasm_shared15_circuit()
+    conversions = [Conversion("autoqasm", "qasm3", autoqasm_to_qasm3)]
+    graph = ConversionGraph(conversions)
+    program = transpile(autoqasm_circuit, "qasm3", conversion_graph=graph, max_path_depth=1)
+
+    assert isinstance(program, str)
+    assert program == normalize_qasm_gate_params(qasm3_shared15_reference())
