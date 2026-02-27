c5b997861b326a7fc2ac9ef8bb03cc81767f2914
TheGupta2012 <harshit.11235@gmail.com>
2026-02-27 16:59:43 +0530

    add: rigetti client in runtime

---
+++ qbraid/runtime/rigetti/__init__.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2026 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+Module for submitting and managing jobs through the Rigetti QCS API.
+
+.. currentmodule:: qbraid.runtime.rigetti
+
+Classes
+--------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+    RigettiProvider
+    RigettiDevice
+    RigettiJob
+
+"""
+
+from .device import RigettiDevice
+from .job import RigettiJob
+from .provider import RigettiProvider
+
+__all__ = ["RigettiDevice", "RigettiProvider", "RigettiJob"]
