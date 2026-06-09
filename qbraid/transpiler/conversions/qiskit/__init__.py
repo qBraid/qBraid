dd3155cd6fba8973d33d92cc1c76dbd737a60c9f
dhanaabhirajk <81916146+dhanaabhirajk@users.noreply.github.com>
2026-06-09 23:38:19 +0530

    Feat: add PennyLane conversions via extras (#1207)
    * add 4 extra
    
    * fix: tests
    
    * fix: dependency
    
    * fix: verion
    
    * version
    
    * fix require_native
    
    * fix: remove and combientests
    
    * fix: toml
    
    * fix: code
    
    * fix: comments
    
    * fix: lint check
    
    * fix: lint
    
    * fix: pennylane
    
    * fix: lint
    
    * fix: lint
    
    * fix: pennylane
    
    * revert: test and lint fix
    
    ---------
    
    Co-authored-by: dhanaabhirajk <Dhana.Kumarasamy@solitontech.local>

--- qbraid/transpiler/conversions/qiskit/__init__.py
+++ qbraid/transpiler/conversions/qiskit/__init__.py
@@ -28,9 +28,10 @@ Functions
    qiskit_to_braket
    qiskit_to_pyqir
    qiskit_to_ionq
+   qiskit_to_pennylane
 
 """
-from .qiskit_extras import qiskit_to_braket, qiskit_to_ionq, qiskit_to_pyqir
+from .qiskit_extras import qiskit_to_braket, qiskit_to_ionq, qiskit_to_pennylane, qiskit_to_pyqir
 from .qiskit_to_qasm2 import qiskit_to_qasm2
 from .qiskit_to_qasm3 import qiskit_to_qasm3
 
@@ -40,4 +41,5 @@ __all__ = [
     "qiskit_to_braket",
     "qiskit_to_pyqir",
     "qiskit_to_ionq",
+    "qiskit_to_pennylane",
 ]
